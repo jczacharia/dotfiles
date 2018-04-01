795f6b8b126bf4978a8e1a26298bd2c61fdc5a93
Jeremy Zacharia <jczacharia@gmail.com>
2018-04-01 13:43:23 -0400

    --bare?

--- git_notes.sh
+++ git_notes.sh
@@ -1,4 +1,12 @@
-git init
+git init #--bare
+
+
+#A bare repository is nothing but the .git folder itself i.e. the contents of a bare repository is same as the contents of .git folder inside your local working repository.
+
+#Use bare repository on a remote server to allow multiple contributors to push their work.
+#Non-bare - The one which has working tree makes sense on the local machine of each contributor of your project.
+
+
 
 git clone <html>
 
