80746c4a8f1e1fa521e38b0fbc9f2c8d8f5fbedd
Jeremy Zacharia <jczacharia@gmail.com>
2018-04-01 13:38:50 -0400

    Added git_notes

---
+++ git_notes.sh
@@ -0,0 +1,16 @@
+git init
+
+git clone <html>
+
+git add .
+
+git push
+
+git status
+
+git checkout <branch>
+
+git log --all --decorate --oneline --graph
+
+#rename branch
+git branch -m <oldname> <newname>
